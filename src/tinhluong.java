ee6a5fc5aabed29b288aa403fb72a0e56ac1e8f0
longsutls <longsutls@gmail.com>
2018-07-26 11:11:28 +0700

    Tinh-luong

---
+++ src/tinhluong.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class tinhluong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Nhap vao he so luong");
+        double hsl = sc.nextDouble();
+        if (hsl > 0 && hsl <= 5) {
+            System.out.println("Nhap vao so nam lam viec");
+            double nlv = sc.nextDouble();
+            double luong = hsl * 4000000  + nlv * 500000;
+            System.out.println("Luong hien tai cua ban la: " + luong + " VND");
+        } else {
+            System.out.println("He so luong khong dung");
+        }
+
+    }
+}
