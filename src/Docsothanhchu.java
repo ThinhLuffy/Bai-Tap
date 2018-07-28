f0b6dfa33dcbc96d0560ef6f882f0352a0029841
longsutls <longsutls@gmail.com>
2018-07-28 20:27:57 +0700

    Bước 2: Đọc các số có 1 chữ số

--- src/Docsothanhchu.java
+++ src/Docsothanhchu.java
@@ -6,5 +6,39 @@ public class Docsothanhchu {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         System.out.println("Nhập vào số bạn muốn đọc thành chữ");
         int number = sc.nextInt();
+        switch (number) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("Nhập 1 số thôi đại ca. chạy lại chương trình đi!");
+        }
     }
 }
