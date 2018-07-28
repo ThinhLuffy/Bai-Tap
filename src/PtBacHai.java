f0926a625094fc024ecc4001e27c67f0c3862e97
longsutls <longsutls@gmail.com>
2018-07-28 19:40:26 +0700

    Trường hợp a != 0 có 3 lựa chọn tìm nghiệm

--- src/PtBacHai.java
+++ src/PtBacHai.java
@@ -11,5 +11,20 @@ public class PtBacHai {
         double b = sc.nextDouble();
         System.out.println("Mời bạn nhập vào giá trị c:");
         double c = sc.nextDouble();
+        double x;
+
+        if (a != 0) {
+            double delta = b * b - 4 * a * c;
+            if (delta > 0) {
+                double x1 = (-b + Math.sqrt(delta)) / 2 * a;
+                double x2 = (-b - Math.sqrt(delta)) / 2 * a;
+                System.out.printf("Phương trình có 2 nghiệm x1 = %f và x2 = %f", x1, x2);
+            } else if (delta == 0) {
+                x = -b / 2 * a;
+                System.out.printf("Phương trình có nghiệm x = %f", x);
+            } else if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            }
+        }
     }
 }
