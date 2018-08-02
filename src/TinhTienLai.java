b0347cf6ac6d21ea06ca7daa5d47fab97133696e
longsutls <longsutls@gmail.com>
2018-08-02 10:23:30 +0700

    Thêm phần nhận dữ liệu người dùng cho 3 biến đã khai báo

--- src/TinhTienLai.java
+++ src/TinhTienLai.java
@@ -7,5 +7,13 @@ public class TinhTienLai {
         double rate = 1.0d;
         int month = 1;
 
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Nhập số tiền gửi");
+        money = sc.nextDouble();
+        System.out.println("Nhập lãi suất");
+        rate = sc.nextDouble();
+        System.out.println("Nhập số tháng gửi");
+        month = sc.nextInt();
+
     }
 }
