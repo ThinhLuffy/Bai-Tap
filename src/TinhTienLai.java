5ee4a9943e570b6d91a46dc62d069c2d69e8e785
longsutls <longsutls@gmail.com>
2018-08-02 10:22:28 +0700

    Khai báo biến

---
+++ src/TinhTienLai.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TinhTienLai {
+    public static void main(String[] args) {
+        double money = 1.0d;
+        double rate = 1.0d;
+        int month = 1;
+
+    }
+}
