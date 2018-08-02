ec7291ba120205ce44811cb354e741600ed37999
longsutls <longsutls@gmail.com>
2018-08-02 11:40:21 +0700

    Hiển thị số nguyên tố nhỏ hơn 100

---
+++ src/DisplayPrimeLess100.java
@@ -0,0 +1,16 @@
+public class DisplayPrimeLess100 {
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là: ");
+        for (int i = 2; i < 100; i++) {
+            boolean check = true;
+            for (int j = 2; j < Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+                System.out.println(i);
+        }
+    }
+}
