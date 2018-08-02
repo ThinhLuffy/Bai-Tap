acea4bde16bbf603b080b3815c45b82f3e9ca6ef
longsutls <longsutls@gmail.com>
2018-08-02 19:39:19 +0700

    khai báo biến và yêu cầu người dùng nhập vào số lượng số nguyên tố cần in ra

---
+++ src/TheFirst20Prime.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TheFirst20Prime {
+    public static void main(String[] args) {
+        System.out.println("Nhập vào lượng số nguyên tố cần in ra: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int count = 0;
+
+    }
+}
+
